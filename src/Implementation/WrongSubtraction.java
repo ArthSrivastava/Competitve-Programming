d1e8bfb2c170175f3eae277d065493c8ac42d330
Arth <arth4701@gmail.com>
2022-03-05 20:15:29 +0530

    Added WrongSubtraction in Implementation

---
+++ src/Implementation/WrongSubtraction.java
@@ -0,0 +1,67 @@
+package Implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class WrongSubtraction {
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader()
+        {
+            br = new BufferedReader(
+                    new InputStreamReader(System.in));
+        }
+
+        String next()
+        {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() { return Integer.parseInt(next()); }
+
+        long nextLong() { return Long.parseLong(next()); }
+
+        double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine()
+        {
+            String str = "";
+            try {
+                str = br.readLine();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+    public static void main(String[] args) {
+        FastReader fr = new FastReader();
+        long n = fr.nextLong();
+        int k = fr.nextInt();
+        while(k-- > 0)
+        {
+            long rem = n % 10;
+            if(rem != 0)
+                n -= 1;
+            else
+                n = n / 10;
+        }
+        System.out.println(n);
+    }
+}
