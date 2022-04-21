9971c5adde59a8c3d81338f061bc2b8bc111f31c
Arth <arth4701@gmail.com>
2022-04-21 23:18:41 +0530

    EatingCandies added

--- src/Implementation/EatingCandies.java
+++ src/Implementation/EatingCandies.java
@@ -5,11 +5,43 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class DiverseSubstring {
+public class EatingCandies {
     public static void main(String[] args) {
         FastReader fr = new FastReader();
-        int n = fr.nextInt();
-        String s = fr.nextLine();
+        int t = fr.nextInt();
+        while (t-- > 0) {
+            int n = fr.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = fr.nextInt();
+            }
+            int low = -1, high = n;
+            int temp = 0;
+            int sum1 = 0, sum2 = 0;
+            int c1 = 0, c2 = 0;
+            while (low <= high) {
+                if (sum1 < sum2) {
+                    low++;
+                    sum1 += arr[low];
+                    c1++;
+                } else if (sum1 > sum2) {
+                    high--;
+                    sum2 += arr[high];
+                    c2++;
+                } else {
+                    low++;
+                    high--;
+                    temp = c1 + c2;
+                    if (low != high && low < n && high > 0) {
+                        sum1 += arr[low];
+                        sum2 += arr[high];
+                        c1++;
+                        c2++;
+                    }
+                }
+            }
+            System.out.println(temp);
+        }
     }
 
     static class FastReader {
