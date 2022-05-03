f1342ac53d35bd9b84a315b90b2973e72877399c
Arth <arth4701@gmail.com>
2022-05-03 19:45:30 +0530

    ParkLighting added in maths

---
+++ src/Maths/ParkLighting.java
@@ -0,0 +1,75 @@
+package Maths;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+//https://codeforces.com/problemset/problem/1358/A
+public class ParkLighting {
+
+    public static void main(String[] args) {
+        FastReader fr = new FastReader();
+        int t = fr.nextInt();
+        while (t-- > 0) {
+            int n = fr.nextInt();
+            int m = fr.nextInt();
+            int count = 0;
+            if (n % 2 == 0) {
+                count +=  m * (n / 2);
+            } else {
+                count +=  m * ((n - 1)/ 2) + Math.ceil(m / 2f);
+            }
+            System.out.println(count);
+        }
+    }
+
+    public int gcd(int a, int b) {
+        if (b==0) return a;
+        return gcd(b,a%b);
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader()
+        {
+            br = new BufferedReader(
+                    new InputStreamReader(System.in));
+        }
+
+        String next()
+        {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() { return Integer.parseInt(next()); }
+
+        long nextLong() { return Long.parseLong(next()); }
+
+        double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine()
+        {
+            String str = "";
+            try {
+                str = br.readLine();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
