940835e452b72de0ae28ffcdc368d8801b14cd49
Arth <arth4701@gmail.com>
2022-05-04 20:35:41 +0530

    MagicalSticks added

---
+++ src/Maths/MagicalSticks.java
@@ -0,0 +1,69 @@
+package Maths;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+//https://codeforces.com/problemset/problem/1371/A
+public class MagicalSticks {
+
+    public static void main(String[] args) {
+        FastReader fr = new FastReader();
+        int t = fr.nextInt();
+        while (t-- > 0) {
+            long n = fr.nextLong();
+            System.out.println((long)Math.ceil(n / 2d));
+//            System.out.println((n + 1) / 2);
+        }
+    }
+
+    public int gcd(int a, int b) {
+        if (b==0) return a;
+        return gcd(b,a%b);
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader()
+        {
+            br = new BufferedReader(
+                    new InputStreamReader(System.in));
+        }
+
+        String next()
+        {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() { return Integer.parseInt(next()); }
+
+        long nextLong() { return Long.parseLong(next()); }
+
+        double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine()
+        {
+            String str = "";
+            try {
+                str = br.readLine();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
