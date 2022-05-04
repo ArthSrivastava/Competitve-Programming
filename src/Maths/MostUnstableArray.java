4a98ae2f130d8412ccea1575248bf7d8ceaf47d4
Arth <arth4701@gmail.com>
2022-05-04 19:42:06 +0530

    MostUnstableArray added

---
+++ src/Maths/MostUnstableArray.java
@@ -0,0 +1,75 @@
+package Maths;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+//https://codeforces.com/problemset/problem/1353/A
+public class MostUnstableArray {
+
+    public static void main(String[] args) {
+        FastReader fr = new FastReader();
+        int t = fr.nextInt();
+        while (t-- > 0) {
+            int n = fr.nextInt();
+            long m = fr.nextLong();
+            if (n == 1) {
+                System.out.println(0);
+            } else if (n == 2) {
+                System.out.println(m);
+            } else {
+                System.out.println(2 * m);
+            }
+        }
+    }
+
+    public int gcd(int a, int b) {
+        if (b==0) return a;
+        return gcd(b,a%b);
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader()
+        {
+            br = new BufferedReader(
+                    new InputStreamReader(System.in));
+        }
+
+        String next()
+        {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() { return Integer.parseInt(next()); }
+
+        long nextLong() { return Long.parseLong(next()); }
+
+        double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine()
+        {
+            String str = "";
+            try {
+                str = br.readLine();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
