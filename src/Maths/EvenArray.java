d5dcf4b7386046230d89c2f72414777fc53e08f8
Arth <arth4701@gmail.com>
2022-05-06 16:13:01 +0530

    EvenArray added

---
+++ src/Maths/EvenArray.java
@@ -0,0 +1,82 @@
+package Maths;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+//https://codeforces.com/problemset/problem/1367/B
+public class EvenArray {
+
+    public static void main(String[] args) {
+        FastReader fr = new FastReader();
+        int t = fr.nextInt();
+        while (t-- > 0) {
+            int n = fr.nextInt();
+            int[] arr = new int[n];
+            int countOdd = 0, countEven = 0;
+            for (int i = 0; i < n; i++) {
+                arr[i] = fr.nextInt();
+                if (arr[i] % 2 == 0 && i % 2 != 0) {
+                    countEven++;
+                } else if(arr[i] % 2 != 0 && i % 2 == 0){
+                    countOdd++;
+                }
+            }
+            if (countEven == countOdd) {
+                System.out.println(countEven);
+            } else {
+                System.out.println(-1);
+            }
+        }
+    }
+
+    public int gcd(int a, int b) {
+        if (b==0) return a;
+        return gcd(b,a%b);
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader()
+        {
+            br = new BufferedReader(
+                    new InputStreamReader(System.in));
+        }
+
+        String next()
+        {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() { return Integer.parseInt(next()); }
+
+        long nextLong() { return Long.parseLong(next()); }
+
+        double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine()
+        {
+            String str = "";
+            try {
+                str = br.readLine();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
