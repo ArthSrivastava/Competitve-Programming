84689135ae15d06040fe0530112de55ffd20475b
Arth <arth4701@gmail.com>
2022-05-30 10:15:28 +0530

    ChatRoom added

---
+++ src/Div2ALadder/ChatRoom.java
@@ -0,0 +1,77 @@
+package Div2ALadder;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class ChatRoom {
+
+    public static void main(String[] args) {
+        FastReader fr = new FastReader();
+        String s = "hello";
+        String str = fr.nextLine();
+        int j = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == s.charAt(j)) {
+                j++;
+                if (j == 5) {
+                    break;
+                }
+            }
+        }
+//        System.out.println(j);
+        if(j == 5) System.out.println("YES");
+        else System.out.println("NO");
+    }
+
+    public static int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader()
+        {
+            br = new BufferedReader(
+                    new InputStreamReader(System.in));
+        }
+
+        String next()
+        {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() { return Integer.parseInt(next()); }
+
+        long nextLong() { return Long.parseLong(next()); }
+
+        double nextDouble()
+        {
+            return Double.parseDouble(next());
+        }
+
+        String nextLine()
+        {
+            String str = "";
+            try {
+                str = br.readLine();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
